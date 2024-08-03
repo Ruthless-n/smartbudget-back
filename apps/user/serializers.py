44bc9206bf9dcb35f7e8700ab6047089cb2a2758
ryanolveir <ryandehallo229@gmail.com>
2024-08-02 23:08:19 -0300

    add: create and get user

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -34,6 +34,7 @@ class UserSerializer(serializers.ModelSerializer):
         )
         return user
 
+
     def update(self, instance, validated_data):
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
