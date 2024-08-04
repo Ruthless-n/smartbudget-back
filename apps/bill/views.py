5c63d3aca32babd6e95911d83d79ed4a0c80c34d
Ruthless-n <ruthcnovais@outlook.com>
2024-08-03 21:12:27 -0300

    feat: add bills crud

--- apps/bill/views.py
+++ apps/bill/views.py
@@ -5,35 +5,56 @@ from apps.core.models import Bill
 from .serializers import BillSerializer
 
 
+    
 
 @api_view(['POST', 'GET'])
 def list_bills(request):
 
     if request.method == 'GET':
-
-        return Response({'message': 'ok'}, status=status.HTTP_200_OK)
+        bills = Bill.objects.all()
+        serializer = BillSerializer(bills, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
         
     if request.method == 'POST':
-        return Response({'message': 'ok'}, status=status.HTTP_200_OK)
-    
-    
-    return Response({'message': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        serializer = BillSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def detail_bills(request, id_bill):
 
-
     if request.method == 'GET':
-
-        return Response({'message': 'ok'}, status=status.HTTP_200_OK)
+        try:
+            bill = Bill.objects.get(id_bill=id_bill)
+        except Bill.DoesNotExist:
+            return Response({'message': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer =  BillSerializer(bill)
+        return Response(serializer.data, status=status.HTTP_200_OK)
         
     if request.method == 'PUT':
-        return Response({'message': 'ok'}, status=status.HTTP_200_OK)
+        try:
+            bill = Bill.objects.get(id_bill=id_bill)
+        except Bill.DoesNotExist:
+            return Response({'message': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = BillSerializer(bill, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
     
 
     if request.method == 'DELETE':
+        try:
+            bill = Bill.objects.get(id_bill=id_bill)
+        except Bill.DoesNotExist:
+            return Response({'message': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        bill.delete()
         return Response({'message': 'ok'}, status=status.HTTP_200_OK)
     
 
