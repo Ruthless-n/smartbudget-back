783baaace06da2ca85956d16f92895ad37e7f3ab
ryanoliveir <ryandehallo229@gmail.com>
2024-08-04 20:29:38 -0300

    add: send all data in bill object

--- apps/bill/serializers.py
+++ apps/bill/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from apps.core.models import Bill
-
+from apps.user.serializers import UserSerializer
+from apps.category.serializer import CategorySerializer
+from apps.status.serializers import StatusSerializer
 
 class BillSerializer(serializers.ModelSerializer):
+
+    responsible = UserSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+    status = StatusSerializer(read_only=True)
+    
     class Meta:
         model = Bill
         fields = '__all__'
