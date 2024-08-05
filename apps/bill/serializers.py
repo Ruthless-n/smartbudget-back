82914e1eaa98d71f54b0e791e67818fd1aff026f
ryanoliveir <ryandehallo229@gmail.com>
2024-08-04 21:12:33 -0300

    fix: bills serializer

--- apps/bill/serializers.py
+++ apps/bill/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps.core.models import Bill
+from apps.core.models import Bill, Category, Status, UserCustomuser
 from apps.user.serializers import UserSerializer
 from apps.category.serializer import CategorySerializer
 from apps.status.serializers import StatusSerializer
@@ -10,6 +10,14 @@ class BillSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     status = StatusSerializer(read_only=True)
     
+    responsible_id = serializers.PrimaryKeyRelatedField(queryset=UserCustomuser.objects.all(), source='responsible', write_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category', write_only=True)
+    status_id = serializers.PrimaryKeyRelatedField(queryset=Status.objects.all(), source='status', write_only=True)
+
     class Meta:
         model = Bill
-        fields = '__all__'
+        fields = [
+            'id_bill', 'bill_name', 'due_date', 'payday', 'amount',
+            'responsible', 'category', 'status',
+            'responsible_id', 'category_id', 'status_id'
+        ]
